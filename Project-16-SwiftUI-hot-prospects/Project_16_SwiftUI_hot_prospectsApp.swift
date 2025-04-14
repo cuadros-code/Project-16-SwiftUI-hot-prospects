37c77ec24137dd353b9cdcf1321d6c83ae32f203
kevin cuadros <kevindavidcuadros@gmail.com>
2025-04-14 10:16:11 -0500

    Initial Commit

---
+++ Project-16-SwiftUI-hot-prospects/Project_16_SwiftUI_hot_prospectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_16_SwiftUI_hot_prospectsApp.swift
+//  Project-16-SwiftUI-hot-prospects
+//
+//  Created by Kevin Cuadros on 14/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Project_16_SwiftUI_hot_prospectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
