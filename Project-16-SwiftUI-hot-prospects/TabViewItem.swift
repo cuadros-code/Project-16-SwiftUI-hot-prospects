dc39a128adc915da45c3c5cac73d180ebf7cdfdf
kevin cuadros <kevindavidcuadros@gmail.com>
2025-04-14 15:30:04 -0500

    Creating tabs with TabView and tabItem()

---
+++ Project-16-SwiftUI-hot-prospects/TabViewItem.swift
@@ -0,0 +1,36 @@
+//
+//  TabViewItem.swift
+//  Project-16-SwiftUI-hot-prospects
+//
+//  Created by Kevin Cuadros on 14/04/25.
+//
+
+import SwiftUI
+
+struct TabViewItem: View {
+    @State private var selectedTab = "One"
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            
+            Button("Show Tab 2") {
+                selectedTab = "Two"
+            }
+            .tabItem {
+                Label("One", systemImage: "star")
+            }
+            .tag("One")
+            
+            Text("Tab 2")
+                .tabItem {
+                    Label("Two", systemImage: "circle")
+                }
+                .tag("Two")
+            
+        }
+    }
+}
+
+#Preview {
+    TabViewItem()
+}
