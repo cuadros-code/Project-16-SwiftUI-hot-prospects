09dadc2bd59570bad769370786310ed60d9f40b7
kevin cuadros <kevindavidcuadros@gmail.com>
2025-04-16 17:25:17 -0500

    Understanding Swift’s Result type

--- Project-16-SwiftUI-hot-prospects/ResultView.swift
+++ Project-16-SwiftUI-hot-prospects/ResultView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct ResultView: View {
+    
+    @State private var output = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(output)
+            .task {
+                await fetchReadingNewVersion()
+            }
+    }
+    
+    func fetchReading() async {
+        do {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            output = "Found \(readings.count) reading"
+            
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func fetchReadingNewVersion() async {
+        let fetchTask = Task {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) reading"
+        }
+        
+        let result = await fetchTask.result
+        
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let error):
+            output = "Error: \(error.localizedDescription)"
+        }
     }
 }
 
