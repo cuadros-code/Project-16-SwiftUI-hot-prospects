39fd1b17a2599f4448570f0b0204493db967ce55
kevin cuadros <kevindavidcuadros@gmail.com>
2025-04-15 20:09:58 -0500

    Result type

---
+++ Project-16-SwiftUI-hot-prospects/ResultView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultView.swift
+//  Project-16-SwiftUI-hot-prospects
+//
+//  Created by Kevin Cuadros on 15/04/25.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultView()
+}
